d017501ff06bca31883a6238ba82300b20721cf8
Bohdan <by.muha@gmail.com>
2021-03-16 20:15:45 +0200

    event listeners for arrows work

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,37 @@
+const images = [...document.getElementById("pic").children];
+const circles = [...document.getElementById("circles").children];
+const getCurrentElement = () =>
+  images.findIndex((img) => img.style.display == "block");
+
+const left = document.getElementById("left");
+left.addEventListener("click", () => {
+  if (getCurrentElement() > 0) renderPicture(getCurrentElement() - 1);
+});
+
+const right = document.getElementById("right");
+right.addEventListener("click", () => {
+  if (getCurrentElement() < images.length - 1)
+    renderPicture(getCurrentElement() + 1);
+});
+
 const initPage = () => {
-  renderPicture(4);
+  renderPicture(0);
 };
 
 const renderPicture = (element) => {
-  const images = [...document.getElementById("pic").children];
-  //   images[0].style.display = "block";
   images.forEach((img) => (img.style.display = "none"));
   images[element].style.display = "block";
 
-  const circles = [...document.getElementById("circles").children];
   circles.forEach((circle) => {
     if (circle.classList.contains("fas")) {
-      circle.classList.delete("fas");
+      circle.classList.remove("fas");
       circle.classList.add("far");
     }
   });
   circles[element].classList.add("fas");
+  
 };
 
 initPage();
+
+console.log(getCurrentElement());
